d0e8d2407dfc68f08fcc06bc1efdeb183129820f
windless1015 <shangtao1989@qq.com>
2022-06-28 19:19:20 -0700

    1. add the application icon downloaded from https://icons8.com/icon/97sp5NU07RFm/sphere with 60*60 pixels for free 2. add this icon to qrc file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <QApplication>
-
+#include <QSurfaceFormat>
+#include <QOpenGLContext>
+#include <QOpenGLFunctions>
+#include <QOffscreenSurface>
 #include "view\mainWindow.h"
-//#include "mainwindow.h"
+
 
 int main (int argc, char** argv)
 {
 	QGuiApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
 	QApplication app(argc, argv);
 
+	QSurfaceFormat fmt;
+	fmt.setRenderableType(QSurfaceFormat::OpenGL);
+	fmt.setVersion(4, 5);
+	fmt.setProfile(QSurfaceFormat::CompatibilityProfile);
+	QSurfaceFormat::setDefaultFormat(fmt);
 
 	MainWindow w;
 	w.show();
-	
 	return app.exec();
 }
--- src/view/mainWindow.cpp
+++ src/view/mainWindow.cpp
@@ -2,10 +2,7 @@
 #include <QMenuBar>
 #include <QToolBar>
 #include <QStatusBar>
-#include <QSurfaceFormat>
-#include <QOpenGLContext>
-#include <QOpenGLFunctions>
-#include <QOffscreenSurface>
+
 #include <QGridLayout>
 #include "ui_mainwindow.h"
 #include "mainWindow.h"
@@ -16,75 +13,20 @@ MainWindow::MainWindow(QWidget *parent)
 {
 	ui->setupUi(this);
 
+	this->setWindowTitle("Sculptor3D");
+	this->setWindowIcon(QIcon(":/sculptor3D.png"));
+
+	w = new MeshViewerWidget(this);
 	initializeUI();
-
-
-	//this->setWindowTitle("My Window");
-	//this->setMinimumSize(500, 400);
-	//this->setWindowFlag(Qt::Widget);
-
-
-	//QMenuBar *menu_bar = new QMenuBar(this);
-	//this->setMenuBar(menu_bar);
-	//QToolBar *tool_bar = new QToolBar(this);                  
-	//this->addToolBar(tool_bar);
-	//QStatusBar *status_bar = new QStatusBar(this);
-	//this->setStatusBar(status_bar);
-
-	//QMenu *file_menu = new QMenu("File(&F)", menu_bar);
-	//QMenu *edit_menu = new QMenu("Edit(&E)", menu_bar);
-	//QMenu *help_menu = new QMenu("Help(&H)", menu_bar);
-
-
-	//QAction *new_action = new QAction("New(&N)");
-	//QAction *open_action = new QAction("Open(&O)");
-	//QAction *save_action = new QAction("Save(&S)");
-
-
-	//new_action->setStatusTip("Open a new file");
-	///*open_action->setStatusTip("open");
-	//save_action->setStatusTip("Save");*/
-
-	//file_menu->addAction(new_action);
-	//file_menu->addAction(open_action);
-	//file_menu->addSeparator();
-	//file_menu->addAction(save_action);
-
-	//edit_menu->addAction("Cut(&T)");
-	//help_menu->addAction("About(&A)");
-
-	//menu_bar->addMenu(file_menu);
-	//menu_bar->addMenu(edit_menu);
-	//menu_bar->addMenu(help_menu);
-
-	//tool_bar->addAction(new_action);
-	//tool_bar->addAction(open_action);
-	//tool_bar->addAction(save_action);
-
-
-
-	QSurfaceFormat fmt;
-	fmt.setRenderableType(QSurfaceFormat::OpenGL);
-	fmt.setVersion(4, 5);
-	fmt.setProfile(QSurfaceFormat::CompatibilityProfile);
-	QSurfaceFormat::setDefaultFormat(fmt);
-
-
-	
-
-	
-
 }
 
 MainWindow::~MainWindow()
 {
-	//delete ui;
+	delete ui;
 }
 
 void MainWindow::initializeUI()
 {
-
-	w = new MeshViewerWidget(this);
 	QHBoxLayout* layout = new QHBoxLayout();
 	layout->addWidget(w);
 	ui->centralwidget->setLayout(layout);
